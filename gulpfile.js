62b5f93ee6ec839772e79e8b5147be67656c96f5
Roma <roma.storoschuk@gmail.com>
2016-08-09 17:30:51 +0300

    final

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 
 
-var gulp        = require('gulp');
-var browserSync = require('browser-sync');
-var sass        = require('gulp-sass');
-var jade        = require('gulp-jade');
-//var plumberNotifier = require('gulp-plumber-notifier');
+var gulp        = require('gulp'),
+    browserSync = require('browser-sync'),
+    sass        = require('gulp-sass'),
+    jade        = require('gulp-jade'),
+    del         = require('del'),
+    autoprefixer = require('gulp-autoprefixer');
 
 
 gulp.task('jade', function() {
@@ -19,6 +20,7 @@ gulp.task('jade', function() {
 gulp.task('sass', function () {
     return gulp.src('./app/sass/*.sass')
         .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
         .pipe(gulp.dest('./app/css'))
         .pipe(browserSync.reload({stream: true}))
 });
@@ -32,6 +34,38 @@ gulp.task('browser-sync', function(){
     });
 });
 
+gulp.task('css-libs', ['sass'], function() {
+    return gulp.src('app/css/nornalize.css') // Выбираем файл для минификации
+        .pipe(cssnano()) // Сжимаем
+        .pipe(rename({suffix: '.min'})) // Добавляем суффикс .min
+        .pipe(gulp.dest('app/css')); // Выгружаем в папку app/css
+});
+
+gulp.task('clean', function() {
+    return del.sync('dist/*');
+});
+
+
+gulp.task('build', ['sass','clean', 'css-libs', 'jade' ], function() {
+
+    var buildCss = gulp.src(['app/css/main.css','app/css/normalize.css' ])
+    .pipe(gulp.dest("dist/css"))
+
+    var buildFonts = gulp.src('app/fonts/FiraSans/*')
+    .pipe(gulp.dest('dist/fonts/FiraSans'))
+
+    var buildJs = gulp.src('app/js/**/*')
+    .pipe(gulp.dest('dist/js'))
+
+    var buildIcons = gulp.src('app/icons/**/*')
+    .pipe(gulp.dest('dist/icons'))
+
+    var buildImg = gulp.src('app/img/**/*')
+    .pipe(gulp.dest('dist/img'))
+
+    var buildHtml = gulp.src('app/*.html')
+    .pipe(gulp.dest('dist'));
+});
 gulp.task('watch', ['sass', 'browser-sync'], function() {
 	gulp.watch('app/sass/*.sass', ['sass']);
 	gulp.watch('app/jade/**/*.jade', ['jade']);
@@ -39,4 +73,7 @@ gulp.task('watch', ['sass', 'browser-sync'], function() {
 	gulp.watch('app/js/*.js', browserSync.reload);
 });
 
+gulp.task('clear', function () {
+    return cache.clearAll();
+})
 gulp.task('default', ['watch']);
